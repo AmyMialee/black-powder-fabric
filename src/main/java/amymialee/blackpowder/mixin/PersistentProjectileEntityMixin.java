a271a6da70992c86892878918a0cb1ea1228bad4
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2021-05-22 02:53:41 +0100

    removed a redirect and removed bintray

--- src/main/java/amymialee/blackpowder/mixin/PersistentProjectileEntityMixin.java
+++ src/main/java/amymialee/blackpowder/mixin/PersistentProjectileEntityMixin.java
@@ -1,22 +1,22 @@
 package amymialee.blackpowder.mixin;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.util.hit.EntityHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PersistentProjectileEntity.class)
 public class PersistentProjectileEntityMixin {
     @Shadow private double damage;
-    @Redirect(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
-    public boolean redirectDamage(Entity entity, DamageSource source, float amount) {
-        if (source.name.contains("bullet")) {
-            return entity.damage(source, (float) damage);
-        } else {
-            return entity.damage(source, amount);
+    @Inject(method = "onEntityHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;damage(Lnet/minecraft/entity/damage/DamageSource;F)Z"))
+    public void redirectDamage(EntityHitResult entityHitResult, CallbackInfo ci) {
+        DamageSource damageSource2 = DamageSource.arrow(((PersistentProjectileEntity)((Object)this)), ((PersistentProjectileEntity)((Object)this)));
+        if (damageSource2.name.contains("bullet")) {
+            int i = (int) damage;
         }
     }
 }
